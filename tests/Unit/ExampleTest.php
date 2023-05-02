293c087bb25602615ca45a13b489289903d45f65
William Tomé <william.tomenh@gmail.com>
2023-05-02 15:24:21 -0300

    refactor(Example): change namespace.

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,20 +1,15 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests;
 
 use Core\Example;
-use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
     public function testShouldReturnTheTesteWord()
     {
-        Mockery::mock(stdClass::class)
-            ->shouldReceive('teste')
-            ->andReturn(true);
-
-        // $example = new Example;
-        // $this->assertEquals('teste', $example->test());
+        $example = new Example;
+        $this->assertEquals('teste', $example->test());
     }
 }
