8eeda454e0b216ee6f80742f34b1ea0ae8b941e2
William Tomé <william.tomenh@gmail.com>
2023-05-03 06:48:58 -0300

    test(Linear Search): implement test cases.

--- src/Core/Algorithms/LinearSearch.php
+++ src/Core/Algorithms/LinearSearch.php
@@ -5,7 +5,22 @@ namespace Core\Algorithms;
 class LinearSearch
 {
     public function __construct(
-        private array $array,
-        private int $number
+        private array $array
     ) {}
+
+    public function getNumbers()
+    {
+        return $this->array;
+    }
+
+    public function search(int $number): int
+    {
+        foreach ($this->array as $index => $value) {
+            if ($value == $number) {
+                return $index;
+            }
+        }
+
+        return -1;
+    }
 }
--- tests/Unit/Core/Algorithms/LinearSearchTest.php
+++ tests/Unit/Core/Algorithms/LinearSearchTest.php
@@ -5,16 +5,50 @@ namespace Tests\Core\Algorithms;
 use PHPUnit\Framework\TestCase;
 use Core\Algorithms\LinearSearch;
 
-class LinearSerchTest extends TestCase
+class LinearSearchTest extends TestCase
 {
+    /**
+     * @dataProvider numbers
+     */
+    public function testAttributes($array)
+    {
+        var_dump($array); die;
+        $linearSearch = new LinearSearch($array);
+
+        $this->assertEquals(
+            $array,
+            $linearSearch->getNumbers()
+        );
+    }
+
     public function testShouldReturnNumberFoundIndex()
     {
         $numbers = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170];
         $numberToSearch = 110;
         $expectedIndex = 6;
 
-        $numberFound = new LinearSearch($numbers, $numberToSearch);
+        $linearSearch = new LinearSearch($numbers);
+        $numberFound = $linearSearch->search($numberToSearch);
 
         $this->assertEquals($expectedIndex, $numberFound);
+    }
+
+    public function testShouldNotReturnNumberFoundIndex()
+    {
+        $numbers = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170];
+        $numberToSearch = 171;
+        $expectedIndex = -1;
+
+        $linearSearch = new LinearSearch($numbers);
+        $numberFound = $linearSearch->search($numberToSearch);
+
+        $this->assertEquals($expectedIndex, $numberFound);
+    }
+
+    public function numbers()
+    {
+        return [
+            [10, 20, 80, 30, 60, 50, 110, 100, 130, 170],
+        ];
     }
 }
